f80735d724cc0fc4fe5d7d48d46495d5b798fcc4
Treavien Holley <70658436+TreaHolley@users.noreply.github.com>
2022-12-07 17:36:10 -0500

    Add files via upload

--- PaintingApp/app/src/main/java/com/example/paintingapp/MainActivity.java
+++ PaintingApp/app/src/main/java/com/example/paintingapp/MainActivity.java
@@ -1,11 +1,40 @@
 package com.example.paintingapp;
-
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import com.github.gcacace.signaturepad.views.SignaturePad;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 import android.os.Bundle;
 import android.view.View;
+//import android.webkit.PermissionRequest;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import java.util.List;
+import java.util.Locale;
 
 import static com.example.paintingapp.display.colorList;
 import static com.example.paintingapp.display.current_brush;
@@ -15,10 +44,97 @@ public class MainActivity extends AppCompatActivity {
     public static Path path = new Path();
     public static Paint paint_brush = new Paint();
 
+    private static String fileName;
+    ImageButton saveButton;
+    SignaturePad pad;
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @NonNull
+    public Uri saveBitmap(@NonNull final Context context, @NonNull final Bitmap bitmap,
+                          @NonNull final Bitmap.CompressFormat format,
+                          @NonNull final String mimeType,
+                          @NonNull final String displayName) throws IOException {
+
+        final ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, displayName);
+        values.put(MediaStore.MediaColumns.MIME_TYPE, mimeType);
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_DCIM);
+
+        final ContentResolver resolver = context.getContentResolver();
+        Uri uri = null;
+
+        try {
+            final Uri contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+            uri = resolver.insert(contentUri, values);
+
+            if (uri == null)
+                throw new IOException("Failed to create new MediaStore record.");
+
+            try (final OutputStream stream = resolver.openOutputStream(uri)) {
+                if (stream == null)
+                    throw new IOException("Failed to open output stream.");
+
+                if (!bitmap.compress(format, 95, stream))
+                    throw new IOException("Failed to save bitmap.");
+            }
+
+            return uri;
+        }
+        catch (IOException e) {
+            if (uri != null) {
+                // Don't leave an orphan entry in the MediaStore
+                resolver.delete(uri, null, null);
+            }
+
+            throw e;
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        saveButton = findViewById(R.id.btnSave);
+
+        askPermission();
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+        String date = format.format(new Date());
+        fileName = String.format("%s%s", date,"painting.png");
+        /*ControlUndo.setOnClickListener {
+
+        }
+        ControlRedo.setOnClickListener {
+
+        }*/
+    }
+
+    private void saveImage() throws IOException {
+
+        Bitmap bitmap = pad.getSignatureBitmap();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            saveBitmap(MainActivity.this, bitmap, Bitmap.CompressFormat.PNG, "image/png", fileName);
+        }
+        Toast.makeText(MainActivity.this, "Painting Saved!", Toast.LENGTH_SHORT).show();
+    }
+
+    private void askPermission() {
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE).withListener((new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                Toast.makeText(MainActivity.this, "Granted!", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+            }
+        })).check();
     }
 
     public void pencil(View view) {
